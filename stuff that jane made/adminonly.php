e28e8af7fd346273846366c57bcd1e41baddf69d
janercat27 <jpbelinhuchak@gmail.com>
2025-09-12 08:45:27 -0300

    Add files via upload

---
+++ stuff that jane made/adminonly.php
@@ -0,0 +1,15 @@
+<?php  include 'top.html';
+session_start();
+if (!empty($_SESSION['username']))
+$con=mysqli_connect('localhost','root','','thebigsecond');
+$sql = "select * from users where username like '$_SESSION[username]' and password like '$_SESSION[password]'";
+$exe=mysqli_query($con,$sql);
+$r2 = mysqli_fetch_array($exe);
+mysqli_close($con);
+if ($r2['user_type'] != 1){
+    header("location:error.html");
+}
+echo"hello this is the admin testing zone";
+
+include 'bottom.html';
+?>
---
+++ stuff that jane made/ascharacter_edit2.php
@@ -0,0 +1,27 @@
+<?php include 'top.html'
+?>
+<?php 
+$id_chara = $_GET['id_chara'];
+$chara_name = $_GET['chara_name'];
+$chara_hp = $_GET['chara_hp'];
+$chara_ws = $_GET['chara_ws'];
+$chara_rs = $_GET['chara_rs'];
+$chara_primary = $_GET['chara_primary'];
+$chara_secondary = $_GET['chara_secondary'];
+$chara_desc = $_GET['chara_desc'];
+$chara_lore = $_GET['chara_lore'];
+$con=mysqli_connect('localhost','root','','thebigsecond');
+$sql = "update as_character set chara_name = '$chara_name',chara_hp = '$chara_hp',chara_ws = '$chara_ws',chara_rs = '$chara_rs',
+chara_primary = '$chara_primary',chara_secondary = '$chara_secondary',chara_desc = '$chara_desc',chara_lore = '$chara_lore'where id_chara=$id_chara";         
+$exe = mysqli_query($con, $sql);
+if ($exe){
+    echo"it worked, go back <a href='aschar.php'>as_character</a>";
+}
+else{
+    echo"1x1x1x1 done fucked something up";
+}
+
+$fecha=mysqli_close($con);
+?>
+<?php include 'bottom.html'
+?>
