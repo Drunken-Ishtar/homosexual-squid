550492773aa5b2caf511e9913d642fa142b49da7
Drunken-Ishtar <sanstheskeleton26052008@gmail.com>
2025-09-18 08:59:20 -0300

    added words to about, slightly edited top and bottom, got rid of unnecessary file, added quick include to the .txt

--- about.php
+++ about.php
@@ -1,7 +1,13 @@
 <?php 
 include "top.html";
 ?>
-    <div> Ough </div>
+    <div> 
+        <p>
+        This is a basic wiki for everything related to DDD, which isnt much.
+        This was made over about 2 months by Ama and Jane, with slight help from Isra and Dink, Who will be the ones in charge of this anyways.
+        If you are having issues, tell me so i can go insane quicker.
+        we ball till we cant ball no more.</p>
+    </div>
 <?php 
 include "bottom.html";
 ?>
