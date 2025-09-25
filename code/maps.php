e4e6d531e6ab4817f1e9586905914a4391096d2a
Drunken-Ishtar <sanstheskeleton26052008@gmail.com>
2025-09-25 10:55:40 -0300

    maps

--- code/maps.php
+++ code/maps.php
@@ -1,12 +1,17 @@
     <?php include 'top.html';?>
 <?php 
-session_start();
 
-    if(empty($_SESSION['username'])) {
-        header('location:login.php');
-    };
     ?>
-
+<?php
+$con = mysqli_connect('localhost','root','','3d_wiki');
+$sql = "select * from as_maps order by map_name ASC";
+$exe = mysqli_query($con, $sql);
+while ($res = mysqli_fetch_array ($exe)){
+    $map = $res['id_map'];
+    $map_name = $res['map_name'];
+    echo"<a href='as_map?id_map=$map'>$map_name</a>"
+};
+?>
 
 
 
