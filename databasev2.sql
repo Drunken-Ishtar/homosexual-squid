fa4fe62238faa10bb8dc040a03f2deb0ce762178
Drunken-Ishtar <sanstheskeleton26052008@gmail.com>
2025-09-25 10:16:02 -0300

    typo

--- databasev2.sql
+++ databasev2.sql
@@ -5,7 +5,7 @@ CREATE TABLE `users` (
   `username_steam` varchar(40),
   `username_minecraft` varchar(40),
   `user_avatar` varchar(20),
-  `user_type ` tinyint not null
+  `user_type` tinyint not null
 );
 
 CREATE TABLE `as_tournament` (
